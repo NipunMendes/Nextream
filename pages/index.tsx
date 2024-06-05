51f50c393335dc1e3348433a30a80f82a9f3b452
Nipun Mendes <nipunmendes@gmail.com>
2024-06-05 11:45:17 +0530

    initial commit
    sessions and logout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,29 @@
+import { getSession, signOut } from "next-auth/react";
+import { NextPageContext } from "next";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
+}
+
 export default function Home() {
   return (
     <>
-      <h1 className="text-2x1 text-blue-800">SLFLIX</h1>
+      <h1 className="text-2x1 text-blue-800">NEXTREAM</h1>
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
+        Sign Out
+      </button>
     </>
   );
 }
