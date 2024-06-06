2b3426dc5807a935ae2818f4ed68a6c412cdbcea
Nipun Mendes <nipunmendes@gmail.com>
2024-06-06 12:00:13 +0530

    initial commit
    Profile

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -2,12 +2,10 @@ import axios from "axios";
 import Input from "@/components/Input";
 import { useCallback, useState } from "react";
 import { signIn } from "next-auth/react";
-import { useRouter } from "next/router";
 
 import { FcGoogle } from "react-icons/fc";
 
 const Auth = () => {
-  const router = useRouter();
   const [name, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -24,15 +22,12 @@ const Auth = () => {
       await signIn("credentials", {
         email,
         password,
-        redirect: false,
-        callbackUrl: "/",
+        callbackUrl: "/profiles",
       });
-
-      router.push("/");
     } catch (error) {
       console.log(error);
     }
-  }, [email, password, router]);
+  }, [email, password]);
 
   const register = useCallback(async () => {
     try {
@@ -92,9 +87,8 @@ const Auth = () => {
               {variant == "login" ? "Sign In" : "Get Started"}
             </button>
             <div className="flex flex-row items-center gap-4 mt-8 justify-center">
-              
               <div
-                onClick={()=>signIn('google',{callbackUrl:'/'})}
+                onClick={() => signIn("google", { callbackUrl: "/profiles" })}
                 className="
               w-10
               h-10
@@ -106,10 +100,10 @@ const Auth = () => {
               cursor-pointer
               hover:opacity-80
               transition
-              ">
-                <FcGoogle/>
+              "
+              >
+                <FcGoogle />
               </div>
-
             </div>
             <p className="text-neutral-400 mt-4 font-semibold">
               {variant == "login" ? "New to Nextream?" : "Already a member?"}
